bd0887ffe9e9f13001745bbede37262670b64368
Zim <h.harisfadzilah@lancaster.ac.uk>
2024-06-05 14:58:47 +0100

    Define basic agent approach

---
+++ AggressiveAgent.py
@@ -0,0 +1,19 @@
+from Agent import Player
+
+
+class RandomAgent(Player):
+
+    def make_selection(self, available_territories: List['Territory']) -> 'Territory':
+        pass
+    def add_infantry(self) -> 'Territory':
+        pass
+
+    def reinforce(self) -> List[Tuple['Territory', int]]:
+        pass
+
+     def invade(self, adjacent_territories: List[Tuple['Territory', List['Territory']]]) -> Tuple['Territory', 'Territory', int]:
+        pass
+    def manoeuvre(self, manoeuverable_territories: List[Tuple['Territory', List['Territory']]]) -> Tuple['Territory', 'Territory', int]:
+        pass
+    def get_player_name(self):
+        pass
